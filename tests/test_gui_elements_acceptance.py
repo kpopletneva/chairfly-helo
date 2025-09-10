21ff8fdb80a546849963e0fcf40705c69435dba4
kseniia.popletneva <kseniia.popletneva@gmail.com>
2025-09-10 11:56:30 -0700

    Added GUI test verifying that clicking the push-to-talk button generates simulated ATC response.

--- tests/test_gui_elements_acceptance.py
+++ tests/test_gui_elements_acceptance.py
@@ -7,3 +7,14 @@ def test_window_defaults(app_instance):
     window, state, controller = app_instance
     assert window._is_fullscreen
     assert not window._title_shown
+
+@pytest.mark.gui
+def test_button_click(app_instance, qtbot):
+    """Test that clicking the push-to-talk button generates simulated ATC response."""
+    window, state, controller = app_instance
+    button = window.button_push_to_talk
+    # 1. Simulate a mouse click on the button
+    qtbot.mouseClick(button, qt_api.QtCore.Qt.MouseButton.LeftButton)
+    # 2. Assert that simulated ATC response appeared
+    #print(window.text_playbook.text()) # ToDo: to be captured by logs
+    assert window.text_playbook.text() in state.playbook
